93125bcf293cbc20ed44ae3251ad5a82f42c3987
Abrahan <abrahamtorres2021@gmail.com>
2022-06-28 12:26:01 -0400

    Se añaden archivos de complejidad algoritmica

---
+++ complejidad_algoritmica.py
@@ -0,0 +1,36 @@
+import time
+import sys  #python tiene un limite de recursion, importando sys se puede cambiar.
+sys.setrecursionlimit(200000)  #Para amplia el límite se usa sys.setrecursionlimit(numero_limite).
+
+def factorial(n):
+    respuesta = 1
+
+    while n > 1:
+        respuesta *= n
+        n -= 1
+
+    return respuesta
+
+def factorial_r(n):
+    if n == 1:
+        return 1
+
+    return n * factorial_r(n - 1)
+
+def main():
+
+    n = 1000
+    
+    comienzo = time.time()
+    factorial(n)
+    final = time.time()
+    print(final - comienzo)
+
+    comienzo = time.time()
+    factorial_r(n)
+    final = time.time()
+    print(final -comienzo)
+
+if __name__ == "__main__":
+    main()
+
---
+++ conteo_abstracto_operacion.py
@@ -0,0 +1,21 @@
+from re import X
+
+
+def f(x):
+
+    respuesta = 0
+
+    for i in range(1000):
+        respuesta += 1
+
+    for i in range(x):
+        respuesta += X
+    
+    for i in range(x):
+        for j in range(x):
+            respuesta += 1
+            respuesta += 1
+
+    return respuesta
+
+    
